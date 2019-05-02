54ecee183dd8302ba4da1fa4d86f90a8b8a8e4fa
camyoh <carlos.mendieta@endava.com>
2019-05-02 09:36:10 -0500

    adding project files

---
+++ ShoppingCart/ShoppingCart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShoppingCart
+//
+//  Created by Carlos Mendieta on 5/2/19.
+//  Copyright © 2019 Carlos Mendieta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShoppingCart/ShoppingCart/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ShoppingCart
+//
+//  Created by Carlos Mendieta on 5/2/19.
+//  Copyright © 2019 Carlos Mendieta. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
